5d189ced10c5d02f2b995576426818012030d6f4
Javier Costa <javierc@bluetrailsoft.com>
2023-12-15 17:30:24 +0100

    Hide content manager + second sidebar

---
+++ packages/core/admin/admin/src/hooks/useCurrentUserIsSuperadmin.ts
@@ -0,0 +1,19 @@
+import { useFetchClient } from "@strapi/helper-plugin"
+import { useEffect, useState } from "react"
+
+export function useCurrentUserIsSuperadmin() {
+    const [isSuperAdmin, setIsSuperadmin] = useState(false)
+    const { get } = useFetchClient()
+
+    useEffect(() => {
+        get('/admin/users/me')
+            .then(({ data }) => {
+                const me = data?.data
+                const _isSuperAdmin = !!me?.roles?.find((role: { code: string }) => role.code === 'strapi-super-admin')
+                console.log(_isSuperAdmin)
+                setIsSuperadmin(_isSuperAdmin)
+            })
+    })
+
+    return isSuperAdmin
+}
